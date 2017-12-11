9052fa703e7a2131ab3b3c480ced246f3da30588
Wladimir J. van der Laan <laanwj@gmail.com>
2017-12-11 16:44:02 +0100

    Merge #11583: Do not make it trivial for inbound peers to generate log entries
    be9f38c Do not make it trivial for inbound peers to generate log entries (Matt Corallo)
    
    Pull request description:
    
      Based on #11580 because I'm lazy.
    
      We should generally avoid writing to debug.log unconditionally for
      inbound peers which misbehave (the peer being about to be banned
      being an exception, since they cannot do this twice).
    
    Tree-SHA512: 8e59c8d08d00b1527951b30f4842d010a4c2fc440503ade112baa2c1b9afd0e0d1c5c2df83dde25183a242af45089cf9b9f873b71796771232ffb6c5fc6cc0cc

--- src/net.cpp
+++ src/net.cpp
@@ -1104,7 +1104,7 @@ void CConnman::AcceptConnection(const ListenSocket& hListenSocket) {
 
     if (IsBanned(addr) && !whitelisted)
     {
-        LogPrintf("connection from %s dropped (banned)\n", addr.ToString());
+        LogPrint(BCLog::NET, "connection from %s dropped (banned)\n", addr.ToString());
         CloseSocket(hSocket);
         return;
     }
