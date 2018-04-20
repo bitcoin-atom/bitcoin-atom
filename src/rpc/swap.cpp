a070d657e017ee1e8926d12fd3f2e9fb8f00a759
virtload <virtload@gmail.com>
2018-04-20 12:31:20 +0700

    [atomic swaps]: write help messages for each rpc method

--- src/rpc/swap.cpp
+++ src/rpc/swap.cpp
@@ -261,7 +261,36 @@ UniValue initiateswap(const JSONRPCRequest& request)
     }
 
     if (request.fHelp || request.params.size() != 2)
-        throw std::runtime_error("");
+        throw std::runtime_error(
+            "initiateswap \"address\" amount\n"
+            "\nThe initiateswap command is performed by the initiator to create the first contract.\n"
+            + HelpRequiringPassphrase(pwallet) +
+            "\nArgumets:\n"
+            "1. \"address\"         (string, required) The recipient address\n"
+            "2. amount              (numeric, required) The value for the swap\n"
+            "\nResult:\n"
+            "{\n"
+            "  \"secret\"           (string) The hex-encoded secret for redemption\n"
+            "  \"secretHash\"       (string) The hex-encoded hash of redemption secret\n" 
+            "  \"contract\": {      (json object) The contract for swap \n"
+            "    \"address\"        (string) The Atom address of the swap contract\n"
+            "    \"scriptHex\"      (string) The contract hex script\n"
+            "    }\n"
+            "  \"contractTx\": {    (json object) The contract transaction\n"
+            "    \"txid\"           (string) The contract transaction id encoded in little-endian hexadecimal\n"
+            "    \"hex\"            (string) The hex-encoded raw transaction with signature(s)\n"
+            "    \"fee\"            (numeric) The amount of the contract transaction fee in BTC\n"
+            "   }\n"
+            "  \"refundTx\": {      (json object) The transaction for refund\n"
+            "    \"txid\"           (string) The refund transaction id encoded in little-endian hexadecimal\n"
+            "    \"hex\"            (string) The hex-encoded refund transaction with signature(s)\n"
+            "    \"fee\"            (numeric) The amount of the refund transaction fee in BTC\n"
+            "   }\n"
+            "}\n"
+            "\nExamples:\n"
+            + HelpExampleCli("initiateswap", "\"mh2nu899jinDFF3XEANbhWXL4RAL5JYDnv\" 1")
+            + HelpExampleRpc("initiateswap", "\"mh2nu899jinDFF3XEANbhWXL4RAL5JYDnv\" 1")
+        );
 
     CTxDestination dest = DecodeDestination(request.params[0].get_str());
     if (dest.which() != 1) {
@@ -295,7 +324,25 @@ UniValue initiateswap(const JSONRPCRequest& request)
 UniValue auditswap(const JSONRPCRequest& request)
 {
     if (request.fHelp || request.params.size() != 2)
-        throw std::runtime_error("");
+        throw std::runtime_error(
+            "auditswap \"hexscript\" \"hextransaction\"\n"
+            "\nThe auditswap command inspects a contract script and parses out the addresses that may claim the output, the locktime, and the secret hash. It also validates that the contract transaction pays to the contract and reports the contract output amount. Each party should audit the contract provided by the other to verify that their address is the recipient address, the output value is correct, and that the locktime is sensible.\n"
+            "\nArgumets:\n"
+            "1. \"hexscript\"       (string, required) The hex-encoded contract\n"
+            "2. \"hextransaction\"  (string, required) The hex-encoded contract transaction\n"
+            "\nResult:\n"
+            "{\n"
+            "  \"contractAddress\"  (string) The Atom address of the swap contract\n" 
+            "  \"contractValue\"    (numeric) The value for the swap\n"
+            "  \"recipientAddress\" (string) The recipient address\n"
+            "  \"refundAddress\"    (string) The address for refund\n"
+            "  \"secretHash\"       (string) The hash of redemption secret\n" 
+            "  \"locktime\"         (numeric) The time for refund unlock\n"
+            "}\n"
+            "\nExamples:\n"
+            + HelpExampleCli("auditswap", "\"6382012088a82085a6637bfc298b2623b94bfeacc8d491edfa0abf3d8bb1c0f2d3b5fa58ad17158876a914109cc30524c19942078ba4356d88e6ef2aa71b546704d408da5ab17576a91423905458d1f6ed5c1efdcf87c9e4e1bc4e57a7796888ac\" \"02000000000101ddbd704186145cc9dcd0b5356d3480efe9b70cf4f2b7d9c919bc6eb506724b0100000000171600143986c86ad67c57f62174d48f159f9de554ba1138feffffff02e0d3f505000000001976a914c929fc3aad535e0d60bb416cc4294a757e31743788ac00e1f5050000000017a91471301e862d2a4e3f01bac5d9255c4d4d81331d7a8702473044022039527fbf8e0a5926d5282138617e1efaec5b2324ddf907623fb185e278b7b71e02203ca51954c1bbccd53927015a384c0236fc5ed2ed5a977adbddaa1c5f7528208a4121022e2622378dc8b9395be551ebaec071fe16ea705d9b018bdecb92ac57e6152e85742d1400\"")
+            + HelpExampleRpc("auditswap", "\"6382012088a82085a6637bfc298b2623b94bfeacc8d491edfa0abf3d8bb1c0f2d3b5fa58ad17158876a914109cc30524c19942078ba4356d88e6ef2aa71b546704d408da5ab17576a91423905458d1f6ed5c1efdcf87c9e4e1bc4e57a7796888ac\" \"02000000000101ddbd704186145cc9dcd0b5356d3480efe9b70cf4f2b7d9c919bc6eb506724b0100000000171600143986c86ad67c57f62174d48f159f9de554ba1138feffffff02e0d3f505000000001976a914c929fc3aad535e0d60bb416cc4294a757e31743788ac00e1f5050000000017a91471301e862d2a4e3f01bac5d9255c4d4d81331d7a8702473044022039527fbf8e0a5926d5282138617e1efaec5b2324ddf907623fb185e278b7b71e02203ca51954c1bbccd53927015a384c0236fc5ed2ed5a977adbddaa1c5f7528208a4121022e2622378dc8b9395be551ebaec071fe16ea705d9b018bdecb92ac57e6152e85742d1400\"")
+        );
 
     std::vector<unsigned char> scriptData(ParseHexV(request.params[0], "hexscript"));
     CScript contractRedeemscript = CScript(scriptData.begin(), scriptData.end());
@@ -358,7 +405,35 @@ UniValue participateswap(const JSONRPCRequest& request)
     }
 
     if (request.fHelp || request.params.size() != 3)
-        throw std::runtime_error("");
+        throw std::runtime_error(
+            "participateswap \"address\" amount \"secrethash\"\n"
+            "\nThe participateswap command is performed by the participant to create a contract on the second blockchain. It operates similarly to initiate but requires using the secret hash from the initiator's contract and creates the contract with a locktime of 24 hours.\n"
+            + HelpRequiringPassphrase(pwallet) +
+            "\nArgumets:\n"
+            "1. \"address\"         (string, required) The recipient address\n"
+            "2. amount              (numeric, required) The value for the swap\n"
+            "3. \"secrethash\"      (string, required) The hex-encoded hash of redemption secret\n"
+            "\nResult:\n"
+            "{\n"
+            "  \"contract\": {      (json object) The contract for swap \n"
+            "    \"address\"        (string) The Atom address of the swap contract\n"
+            "    \"scriptHex\"      (string) The hex-encoded contract\n"
+            "    }\n"
+            "  \"contractTx\": {    (json object) The contract transaction\n"
+            "    \"txid\"           (string) The contract transaction id encoded in little-endian hexadecimal\n"
+            "    \"hex\"            (string) The hex-encoded contract transaction\n"
+            "    \"fee\"            (numeric) The amount of the contract transaction fee in BTC\n"
+            "   }\n"
+            "  \"refundTx\": {      (json object) The transaction for refund\n"
+            "    \"txid\"           (string) The refund transaction id encoded in little-endian hexadecimal\n"
+            "    \"hex\"            (string) The hex-encoded refund transaction\n"
+            "    \"fee\"            (numeric) The amount of the refund transaction fee in BTC\n"
+            "   }\n"
+            "}\n"
+            "\nExamples:\n"
+            + HelpExampleCli("participateswap", "\"mxhBqMzeFc4N6ugGFMi5A4gRGD6yZ7HsW5\" 2 \"3fbf99fcb5690013e742991b5d43f62ac897ecb8494a33feaf6f76ff16b66d9f\"")
+            + HelpExampleRpc("participateswap", "\"mxhBqMzeFc4N6ugGFMi5A4gRGD6yZ7HsW5\" 2 \"3fbf99fcb5690013e742991b5d43f62ac897ecb8494a33feaf6f76ff16b66d9f\"")
+        );
 
     CTxDestination dest = DecodeDestination(request.params[0].get_str());
     if (dest.which() != 1) {
@@ -397,7 +472,24 @@ UniValue redeemswap(const JSONRPCRequest& request)
     }
 
     if (request.fHelp || request.params.size() != 3)
-        throw std::runtime_error("");
+        throw std::runtime_error(
+            "redeemswap \"hexscript\" \"hextransaction\" \"secret\"\n"
+            "\nThe redeemswap command is performed by both parties to redeem coins paid into the contract created by the other party. Redeeming requires the secret and must be performed by the initiator first. Once the initiator's redemption has been published, the secret may be extracted from the transaction and the participant may also redeem their coins.\n"
+            + HelpRequiringPassphrase(pwallet) +
+            "\nArgumets:\n"
+            "1. \"hexscript\"       (string, required) The hex-encoded contract\n"
+            "2. \"hextransaction\"  (string, required) The hex-encoded contract transaction\n"
+            "3. \"secret\"          (string, required) The hex-encoded secret for redemption\n"
+            "\nResult:\n"
+            "{\n"
+            "  \"txid\"             (string) The redemption transaction id encoded in little-endian hexadecimal\n" 
+            "  \"hex\"              (string) The hex-encoded redemption transaction\n"
+            "  \"Redeem fee\"       (numeric) The amount of the redeem transaction fee in BTC\n"
+            "}\n"
+            "\nExamples:\n"
+            + HelpExampleCli("redeemswap", "\"6382012088a82085a6637bfc298b2623b94bfeacc8d491edfa0abf3d8bb1c0f2d3b5fa58ad17158876a914109cc30524c19942078ba4356d88e6ef2aa71b546704d408da5ab17576a91423905458d1f6ed5c1efdcf87c9e4e1bc4e57a7796888ac\" \"02000000000101ddbd704186145cc9dcd0b5356d3480efe9b70cf4f2b7d9c919bc6eb506724b0100000000171600143986c86ad67c57f62174d48f159f9de554ba1138feffffff02e0d3f505000000001976a914c929fc3aad535e0d60bb416cc4294a757e31743788ac00e1f5050000000017a91471301e862d2a4e3f01bac5d9255c4d4d81331d7a8702473044022039527fbf8e0a5926d5282138617e1efaec5b2324ddf907623fb185e278b7b71e02203ca51954c1bbccd53927015a384c0236fc5ed2ed5a977adbddaa1c5f7528208a4121022e2622378dc8b9395be551ebaec071fe16ea705d9b018bdecb92ac57e6152e85742d1400\" \"50ab577bd16fc7e4446ac94dbf0020da01a9e31c2b3e8cccb842f5ce284f671e\"")
+            + HelpExampleRpc("redeemswap", "\"6382012088a82085a6637bfc298b2623b94bfeacc8d491edfa0abf3d8bb1c0f2d3b5fa58ad17158876a914109cc30524c19942078ba4356d88e6ef2aa71b546704d408da5ab17576a91423905458d1f6ed5c1efdcf87c9e4e1bc4e57a7796888ac\" \"02000000000101ddbd704186145cc9dcd0b5356d3480efe9b70cf4f2b7d9c919bc6eb506724b0100000000171600143986c86ad67c57f62174d48f159f9de554ba1138feffffff02e0d3f505000000001976a914c929fc3aad535e0d60bb416cc4294a757e31743788ac00e1f5050000000017a91471301e862d2a4e3f01bac5d9255c4d4d81331d7a8702473044022039527fbf8e0a5926d5282138617e1efaec5b2324ddf907623fb185e278b7b71e02203ca51954c1bbccd53927015a384c0236fc5ed2ed5a977adbddaa1c5f7528208a4121022e2622378dc8b9395be551ebaec071fe16ea705d9b018bdecb92ac57e6152e85742d1400\" \"50ab577bd16fc7e4446ac94dbf0020da01a9e31c2b3e8cccb842f5ce284f671e\"")
+        );
 
     std::vector<unsigned char> contract(ParseHexV(request.params[0], "hexscript"));
 
@@ -489,7 +581,20 @@ UniValue redeemswap(const JSONRPCRequest& request)
 UniValue extractsecret(const JSONRPCRequest& request)
 {
     if (request.fHelp || request.params.size() != 2)
-        throw std::runtime_error("");
+        throw std::runtime_error(
+            "extractsecret \"hextransaction\" \"secrethash\"\n"
+            "\nThe extractsecret command is used by the participant to extract the secret from the initiator's redemption transaction. With the secret known, the participant may claim the coins paid into the initiator's contract.\n"
+            "\nArgumets:\n"
+            "1. \"hextransaction\"  (string, required) The hex-encoded redemption transaction\n"
+            "2. \"secrethash\"      (string, required) The hex-encoded hash of redemption secret\n"
+            "\nResult:\n"
+            "{\n"
+            "  \"secret\"           (string) The hex-encoded secret for redemption\n" 
+            "}\n"
+            "\nExamples:\n"
+            + HelpExampleCli("extractsecret", "\"02000000019d06cfd5d6500bb810d726e07e4000f54076a11a3ea317ca2f2126baba95c8b600000000ef473044022001a1e090631efd01fa6a53fcc1a71b963f9096d35a8ec717771a8b0f3d240bd702203309b438b58dc990a09e820fd6d9110b331fefd5d4f2ee22c9ca4bb0b12d9daf412103089998f1ac124f16fb215f7d4cc5a428cc6acf5d7eddf1599f7a693409877f582062ad2f3179961e78e2851d5387622aa9faff6ae1266aeb5610de5f91ebb29471514c616382012088a8203fbf99fcb5690013e742991b5d43f62ac897ecb8494a33feaf6f76ff16b66d9f8876a914bc6999b404d7d393f13a242f03ebd40c17c08a676704fba4d95ab17576a91470231e7988280299558c1b5c0e83bcbe4df1c3156888acffffffff014ca8eb0b000000001976a9144536e14d69c8d3b0b090a02a2185a4238dcd226e88acfba4d95a\" \"3fbf99fcb5690013e742991b5d43f62ac897ecb8494a33feaf6f76ff16b66d9f\"")
+            + HelpExampleRpc("extractsecret", "\"02000000019d06cfd5d6500bb810d726e07e4000f54076a11a3ea317ca2f2126baba95c8b600000000ef473044022001a1e090631efd01fa6a53fcc1a71b963f9096d35a8ec717771a8b0f3d240bd702203309b438b58dc990a09e820fd6d9110b331fefd5d4f2ee22c9ca4bb0b12d9daf412103089998f1ac124f16fb215f7d4cc5a428cc6acf5d7eddf1599f7a693409877f582062ad2f3179961e78e2851d5387622aa9faff6ae1266aeb5610de5f91ebb29471514c616382012088a8203fbf99fcb5690013e742991b5d43f62ac897ecb8494a33feaf6f76ff16b66d9f8876a914bc6999b404d7d393f13a242f03ebd40c17c08a676704fba4d95ab17576a91470231e7988280299558c1b5c0e83bcbe4df1c3156888acffffffff014ca8eb0b000000001976a9144536e14d69c8d3b0b090a02a2185a4238dcd226e88acfba4d95a\" \"3fbf99fcb5690013e742991b5d43f62ac897ecb8494a33feaf6f76ff16b66d9f\"")
+        );
 
     CMutableTransaction tx;
     if (!DecodeHexTx(tx, request.params[0].get_str(), true)) {
@@ -535,7 +640,23 @@ UniValue refundswap(const JSONRPCRequest& request)
     }
 
     if (request.fHelp || request.params.size() != 2)
-        throw std::runtime_error("");
+        throw std::runtime_error(
+            "refundswap \"hexscript\" \"hextransaction\"\n"
+            "\nThe refundswap command is used to create and send a refund of a contract transaction. While the refund transaction is created and displayed during contract creation in the initiate and participate steps, the refund can also be created after the fact in case there was any issue sending the transaction (e.g. the contract transaction was malleated or the refund fee is now too low).\n"
+            + HelpRequiringPassphrase(pwallet) +
+            "\nArgumets:\n"
+            "1. \"hexscript\"       (string, required) The hex-encoded contract\n"
+            "2. \"hextransaction\"  (string, required) The hex-encoded contract transaction\n"
+            "\nResult:\n"
+            "{\n"
+            "  \"txid\"             (string) The redemption transaction id encoded in little-endian hexadecimal\n" 
+            "  \"hex\"              (string) The hex-encoded refund transaction with signature(s)\n"
+            "  \"fee\"              (numeric) The amount of the refund transaction fee in BTC\n"
+            "}\n"
+            "\nExamples:\n"
+            + HelpExampleCli("refundswap", "\"6382012088a82043098e913574fd73052dc4916c14641d2a7d3dc51de533e7ceb1ece447cd53018876a914bf5d762453cadfaa953ddbb334d2e9179c00c4e167041765d85ab17576a914c3c1bac44ebf6394cfc9dbaf22b13967193c2fdd6888ac\" \"02000000017d6299fc712683b1c383b15e98459561a2c1d1e554eda5ca0b6849ca96030185000000006b483045022100dfdd309af06493ae16b5f8b44bb1407639d964e4df1b7b3520b845b95731ef50022066305f28dcde0f9a28c961016408f603ce9d0d0ee1bc0ffcd250cd58baa867e141210310e244d5f11d7b6199c2e2b1099c864ec27556ff966c895612124c454c378e7ffeffffff0200b4c4040000000017a91449e1ee8259d8fe2f5fd733e0e1c7fa45cb0c8e9b87cce22607000000001976a914afbbf807011cc924043fdfc0f40ab8557f12a8dc88acdf2d1400\"")
+            + HelpExampleRpc("refundswap", "\"6382012088a82043098e913574fd73052dc4916c14641d2a7d3dc51de533e7ceb1ece447cd53018876a914bf5d762453cadfaa953ddbb334d2e9179c00c4e167041765d85ab17576a914c3c1bac44ebf6394cfc9dbaf22b13967193c2fdd6888ac\" \"02000000017d6299fc712683b1c383b15e98459561a2c1d1e554eda5ca0b6849ca96030185000000006b483045022100dfdd309af06493ae16b5f8b44bb1407639d964e4df1b7b3520b845b95731ef50022066305f28dcde0f9a28c961016408f603ce9d0d0ee1bc0ffcd250cd58baa867e141210310e244d5f11d7b6199c2e2b1099c864ec27556ff966c895612124c454c378e7ffeffffff0200b4c4040000000017a91449e1ee8259d8fe2f5fd733e0e1c7fa45cb0c8e9b87cce22607000000001976a914afbbf807011cc924043fdfc0f40ab8557f12a8dc88acdf2d1400\"")
+        );
 
     std::vector<unsigned char> scriptData(ParseHexV(request.params[0], "hexscript"));
     CScript contractRedeemscript = CScript(scriptData.begin(), scriptData.end());
